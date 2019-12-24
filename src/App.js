3da98658aef38fee5cc884d68edbaff94877c40e
Alex <plagun98@gmail.com>
2019-12-24 17:22:09 +0200

    fetchData in App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import List from './components/List';
 import {Router} from 'react-router-dom';
@@ -9,11 +10,22 @@ const history = createBrowserHistory();
 
 const App = (props) => {
 
-console.log(props.productsStorage)
+  const [products, setProducts] = useState([]);
+  const url = 'https://demo9165932.mockable.io/products';
+
+  useEffect(() => {
+
+      const fetchData = async () => {
+          const result = await axios(url);
+          setProducts(result.data.products);
+      };
+      fetchData();
+
+  }, [url]);
 
   return (
     <Router history={history}>
-      <List/>
+      <List products={products}/>
     </Router>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import Menu from './Menu';
 import AllProducts from './AllProducts';
 import BabyProducts from './BabyProducts';
@@ -8,30 +7,17 @@ import HomeKitchen from './HomeKitchen';
 import HealthCare from './HealthCare';
 import {Route, Switch} from 'react-router-dom';
 
-const List = () => {
-
-    const [products, setProducts] = useState([]);
-    const url = 'https://demo9165932.mockable.io/products';
-
-    useEffect(() => {
-
-        const fetchData = async () => {
-            const result = await axios(url);
-            setProducts(result.data.products);
-        };
-        fetchData();
-
-    }, [url]);
+const List = (props) => {
 
     return (
         <div className="container">
             <Menu/>
             <Switch>
-                <Route exact path="/" render={() => <AllProducts products={products}/>} />
-                <Route path="/babyproducts" render={() => <BabyProducts products={products}/>}/>
-                <Route path="/sportsoutdoors" render={() => <SportsOutdoors products={products}/>}/>
-                <Route path="/homekitchen" render={() => <HomeKitchen products={products}/>}/>
-                <Route path="/healthcare" render={() => <HealthCare products={products}/>}/>
+                <Route exact path="/" render={() => <AllProducts products={props.products}/>} />
+                <Route path="/babyproducts" render={() => <BabyProducts products={props.products}/>}/>
+                <Route path="/sportsoutdoors" render={() => <SportsOutdoors products={props.products}/>}/>
+                <Route path="/homekitchen" render={() => <HomeKitchen products={props.products}/>}/>
+                <Route path="/healthcare" render={() => <HealthCare products={props.products}/>}/>
             </Switch>
         </div>
     )
